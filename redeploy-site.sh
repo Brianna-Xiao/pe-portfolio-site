a0fff7e9b83d279f303c43bb772a4e20e169c5a7
Brianna Xiao <briannaxiao5@gmail.com>
2026-07-13 00:03:00 -0400

    Replace tmux deployment with systemd service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
-tmux kill-server 2>/dev/null
+cd /root/pe-portfolio-site
 
-cd ~/pe-portfolio-site || exit 1
-
-git fetch
+git fetch origin
 git reset --hard origin/main
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s portfolio "cd ~/pe-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl daemon-reload
+systemctl restart myportfolio.service
+systemctl status myportfolio.service --no-pager
